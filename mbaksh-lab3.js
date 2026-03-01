726a5332374d5527f1f36b504df33877ab341451
Mudassir-13 <mbaksh@hawk.iit.edu>
2026-03-01 13:02:40 -0600

    added a countVowels function which counts the number of numbers in a string

--- mbaksh-lab3.js
+++ mbaksh-lab3.js
@@ -1,6 +1,6 @@
 // M Baksh
 // ITMD 441-02 Undergraduate Student
-
+// ----------------------------------------------
 
 //Exercise #1
 function minMaxAverage(array) {
@@ -30,3 +30,23 @@ function minMaxAverage(array) {
 minMaxAverage([2, 5, 23, 6, 9, 4, 30, 3]);
 minMaxAverage([6, 5, 3, 5, 10, 12, 8, 9]);
 minMaxAverage([-5, 9, 14, 10, 22, 7, 5, 8]);
+
+
+// ----------------------------------------------
+//Exercise #2
+function countVowels(string) {
+    let count = 0;
+    let vowels = "aeiouAEIOU";
+
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            count++;
+        }
+    }
+
+    console.log(string + ": " + count + " vowels");
+}
+
+countVowels("Hello World");
+countVowels("JavaScript is fun");
+countVowels("Programming is awesome");
